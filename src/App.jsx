254316f0e38861529a355b939e5502b70688535a
Joey <jdavidmangula@gmail.com>
2026-05-13 02:18:04 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
+import AddMemberForm from "./components/AddMemberForm";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-
+      <AddMemberForm />
     </div>
   );
 }
---
+++ src/components/AddMemberForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddMemberForm = () => {
+  return (
+    <div className="bg-white p-4 rounded-lg shadow-md">
+      <h2 className="text-xl font-bold text-gray-800">Add Member</h2>
+    </div>
+  )
+}
+
+export default AddMemberForm;
