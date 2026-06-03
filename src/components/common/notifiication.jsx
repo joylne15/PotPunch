749813a7930c48cf2a99f8286de8e4e324126a3e
Joey <jdavidmangula@gmail.com>
2026-06-03 17:20:05 +0300

    remove notification

--- src/components/common/notifiication.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";  
-
-export default function Notification({ type = "info", message, className = "" }) 
-  const colors = {
-    info: "bg-blue-100 text-blue-800",
-    success: "bg-green-100 text-green-800",
-    warning: "bg-yellow-100 text-yellow-800",
-    error: "bg-red-100 text-red-800",
-  };                
